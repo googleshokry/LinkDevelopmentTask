86ff6fde7640acda0fd5c9cea966f673b394acdd
shokryhassan <s.shokry@muslimdo.com>
2023-08-26 10:15:42 +0300

    create new api health for catalog and email services and create postman collection

--- catalog-service/routes/api.php
+++ catalog-service/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('health', \Spatie\Health\Http\Controllers\SimpleHealthCheckController::class);
+});
