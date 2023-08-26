d20a4246155f311bad4750ecd9175da012991843
shokryhassan <s.shokry@muslimdo.com>
2023-08-26 10:12:49 +0300

    create new api health for catalog and email services and create postman collection

--- email-service/app/Providers/AppServiceProvider.php
+++ email-service/app/Providers/AppServiceProvider.php
@@ -3,6 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\CpuLoadHealthCheck\CpuLoadCheck;
+use Spatie\Health\Checks\Checks\CacheCheck;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\DebugModeCheck;
+use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Facades\Health;
+use Spatie\Health\Checks\Checks\QueueCheck;
+use Spatie\Health\Checks\Checks\ScheduleCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +21,18 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        Health::checks([
+            EnvironmentCheck::new(),
+            CacheCheck::new(),
+            CpuLoadCheck::new()
+                ->failWhenLoadIsHigherInTheLast5Minutes(2.0)
+                ->failWhenLoadIsHigherInTheLast15Minutes(1.5),
+            DatabaseCheck::new(),
+            DebugModeCheck::new(),
+//            QueueCheck::new(),
+//            ScheduleCheck::new(),
+//            UsedDiskSpaceCheck::new(),
+        ]);
     }
 
     /**
